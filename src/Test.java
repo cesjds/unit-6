82b35f81293193617e8919ffe0f6aaacea433554
SkyQuiet12 <yoavamit@icloud.com>
2022-02-16 15:01:00 -0500

    Final unit test. No more changes will be made

--- src/Test.java
+++ src/Test.java
@@ -1,23 +1,35 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Test {
+
     public static void main(String[] args) {
-        String[] word = new String[8];
+        JFrame frame = new JFrame();
+        Container pane = frame.getContentPane();
+        pane.setLayout(new BorderLayout());
 
-        word[0] = String.valueOf('c');
-        word[1] = String.valueOf('o');
-        word[2] = String.valueOf('m');
-        word[3] = String.valueOf('p');
-        word[4] = String.valueOf('u');
-        word[5] = String.valueOf('t');
-        word[6] = String.valueOf('e');
-        word[7] = String.valueOf('r');
+        // slider to control horizontal rotation
+        JSlider headingSlider = new JSlider(0, 360, 180);
+        pane.add(headingSlider, BorderLayout.SOUTH);
 
-        System.out.println(word[4]);
-       // System.out.println(word[4+5]);
-        System.out.println(word[4] + word[5]);
-        //System.out.println(word[16/2]);
-        System.out.println(word[0%4]);
+        // slider to control vertical rotation
+        JSlider pitchSlider = new JSlider(SwingConstants.VERTICAL, -90, 90, 0);
+        pane.add(pitchSlider, BorderLayout.EAST);
 
+        // panel to display render results
+        JPanel renderPanel = new JPanel() {
+            public void paintComponent(Graphics g) {
+                Graphics2D g2 = (Graphics2D) g;
+                g2.setColor(Color.BLACK);
+                g2.fillRect(0, 0, getWidth(), getHeight());
 
+                // rendering magic will happen here
+                
+            }
+        };
+        pane.add(renderPanel, BorderLayout.CENTER);
 
+        frame.setSize(400, 400);
+        frame.setVisible(true);
     }
 }
